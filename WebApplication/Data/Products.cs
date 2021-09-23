2b9bed920b3dc71c85d38dbf53654981f2240a8c
Justus Omondi <85983194+Justus446@users.noreply.github.com>
2021-09-24 00:14:47 +0300

    Add project files.

---
+++ WebApplication/Data/Products.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace WebApplication.Data
+{
+    public class Products
+    {
+        [JsonPropertyName("product_id")]
+        public string productId { get; set; }
+
+
+        [JsonPropertyName("type")]
+        public string productType { get; set; }
+
+        [JsonPropertyName("name")]
+        public string productName { get; set; }
+
+
+        [JsonPropertyName("description")]
+        public string productDescription { get; set; }
+
+
+        [JsonPropertyName("price")]
+        public string productPrice { get; set; }
+
+        [JsonPropertyName("img")]
+        public string productImage { get; set; }
+
+    }
+}
---
+++ WebApplication/Data/ProductsService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+// HttpClient Http
+using WebApplication.Data;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WebApplication.Data
+{
+    public class ProductsService
+    {
+        //public static string jsonFilePath = Http.GetFromJsonAsync<IEnumerable<Products>>("products.json")
+        // public static string jsonString = r.ReadToEnd();
+        // public static string jsonFilePath = "products.json";
+        // ReadAllText("wwwroot/Data/products.json");
+        //public static string json = System.IO.File.ReadAllText("C:/Users/Toshiba/source/repos/WebApplication/WebApplication/wwwroot/Data/products.json");
+        //public static string json = System.IO.File.ReadAllText("products.json");
+        /*
+        public ProductsService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+
+        private string JsonFile
+        {
+            //get { return Path.Combine(WebHostEnvironment.WebRootPath, "Data", "products.json"); }
+            get { return JObject o1 = JObject.Parse(File.ReadAllText(@"c:\videogames.json")); }
+
+        }
+
+
+
+
+        public IEnumerable<Products> GetProductsAsync()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFile))
+            {
+                return JsonSerializer.Deserialize<IEnumerable<Products>>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true});
+            }
+        }*/
+        public List<Products> GetProductsAsync()
+        {
+            using (StreamReader r = new StreamReader(@"products.json"))
+            {
+                string jsonFileReader = r.ReadToEnd();
+                return JsonSerializer.Deserialize<List<Products>>(jsonFileReader, 
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+
+            }
+        }
+
+    }
+}
